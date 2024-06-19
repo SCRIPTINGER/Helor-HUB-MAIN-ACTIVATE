956b37a060b2b230c9c892ce06603e934dc1bd4e
SCRIPTINGER <scriptingrobloxinger@gmail.com>
2024-06-19 18:12:28 +0700

    Update Premium.lua

--- Premium.lua
+++ Premium.lua
@@ -1,1 +1,7 @@
-
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Window = OrionLib:MakeWindow({Name = "Helor HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "Helor HUB FILES"})
+local Tab = Window:MakeTab({
+	Name = "Premium",
+	Icon = "rbxassetid://4483345998",
+	PremiumOnly = true
+})
