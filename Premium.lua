b28a64c65418bbff57dba02d063c998e28e05c9e
HELORER <scriptingrobloxinger@gmail.com>
2024-06-20 14:07:03 +0700

    Update and rename Premium.lua to 1.0,2

--- Premium.lua
+++
@@ -1,7 +0,0 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
-local Window = OrionLib:MakeWindow({Name = "Helor HUB", HidePremium = false, SaveConfig = true, ConfigFolder = "Helor HUB FILES"})
-local Tab = Window:MakeTab({
-	Name = "Premium",
-	Icon = "rbxassetid://4483345998",
-	PremiumOnly = true
-})
